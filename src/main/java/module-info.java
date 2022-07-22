eb73226e7f3d037be2c4eb782b5f08b050522257
PandaNinjas <admin@malwarefight.gq>
2022-07-21 22:06:30 -0700

    v1.1.0
    Changelog:
        Refractoring the version of the obfuscator to the end
        Adding the advanced module
        Pulling latest core deobfuscator version
        Minor changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module NarumiDeobfGUI {
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.controls;
-    requires Deobfuscator;
+//    requires Deobfuscator;
     requires org.objectweb.asm;
+    requires Deobfuscator.edd277a;
     opens gq.malwarefight.narumiideobfgui to javafx.fxml;
+    exports gq.malwarefight.narumiideobfgui to javafx.graphics;
 }
