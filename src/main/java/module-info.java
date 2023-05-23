27377baf4b52bf2632b4de85d6751929dea60356
PandaNinjas <admin@malwarefight.wip.la>
2023-05-23 14:32:04 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module NarumiDeobfGUI {
     requires javafx.graphics;
     requires javafx.controls;
     requires org.objectweb.asm;
-    requires Deobfuscator;
+    requires Deobfuscator.fc5f31f;
     opens gq.malwarefight.narumiideobfgui to javafx.fxml;
     exports gq.malwarefight.narumiideobfgui to javafx.graphics;
 }
